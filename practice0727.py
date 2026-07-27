055f7befb8627908b44c12f93c43e020ded57d12
R-sparrow <1171003197@qq.com>
2026-07-27 23:51:23 +0800

    上传0727练习文件

---
+++ practice0727.py
@@ -0,0 +1,26 @@
+import urllib.request
+import socket
+import urllib.errozr
+from urllib import request, parse
+
+'''
+response = urllib.request.urlopen('https://www.httpbin.org/get', timeout=0.1)
+print (response.read())
+'''
+
+'''
+try:
+    response = urllib.request.urlopen('https://www.httpbin.org/get', timeout=0.1)
+except urllib.error.URLError as e:                           #含义：捕获所有由网络请求失败引发的 urllib.error.URLError 错误，并将捕捉到的异常对象命名为 e
+    if isinstance(e.reason, socket.timeout):                 #isinstance(A, B)：Python 内置函数，用来检查对象 A 是否是类/类型 B 的实例。
+        print('TIME OUT')
+'''
+
+
+url = 'https://www.httpbin.org/post'
+headers = {
+    'User-Agent': 'Mozilla/4.0' (compatible; MSIE 5.5; Windows NT),
+    'Host': 'www.httpbin.org'
+}
+dict = bytes(parse.urlencode(dict), encoding='utf-8')
+req = request.Request(url=url, data=data, headers=headers)
